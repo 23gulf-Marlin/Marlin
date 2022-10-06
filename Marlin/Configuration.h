5109a291ecea4ea595bab3a2b6780012a7dce188
waynekathy <waynekathy@outlook.com>
2022-10-05 19:46:59 -0500

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -60,7 +60,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(9/07/22 Artillery SX1, SKR3 EZ bugfix 2.1.x )" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(10/05/22 Artillery SX1, SKR3 EZ bugfix 2.1.x )" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
